4f6237fa70d213677eab9c727951997aaca00512
zhengxs <zhengxs2018@gmail.com>
2021-09-23 11:27:47 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 
+import pkg from './package.json'
+
+// 剔除所有开发和生产依赖
+const deps = [
+  Object.keys(pkg.dependencies || {}),
+  Object.keys(pkg.devDependencies || {}),
+]
+
 export default defineConfig({
   input: "src/index.js",
   output: {
     file: "dist/index.js",
     format: "cjs",
   },
+  external: Array.from(new Set(deps.flat())),
   plugins: [nodeResolve(), commonjs()],
-});
+})
